a88bf6299e9940b009dfd3b2c57ba3cd637fe70f
wolves7688 <Danielskerman2000@gmail.com>
2025-10-18 10:06:32 +0100

    add theme to match portfolio

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
-
+};
