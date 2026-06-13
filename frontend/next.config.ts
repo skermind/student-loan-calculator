88fcd552d5207a974714a6c35f82a961dad730e0
wolves7688 <Danielskerman2000@gmail.com>
2026-06-13 08:17:45 +0100

    server configs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://backend:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
